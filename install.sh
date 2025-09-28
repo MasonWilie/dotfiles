952f3c29d0b81da8b4edf0eee28c9e0935a6992d
Mason <masonwilie@gmail.com>
2025-09-28 12:20:11 -0600

    Init

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+DOTFILES_URL="https://github.com/MasonWilie/dotfiles.git"
+DOTFILES_DIR="${HOME}/.dotfiles"
+
+link_files() {
+	pushd $HOME
+	
+	ln -sf ${DOTFILES_DIR}/emacs/emacs_config .emacs
+
+	popd
+}
+
+main() {
+	if [ -d "$DOTFILES_DIR" ]; then
+		rm -rf $DOTFILES_DIR
+	fi
+
+	git clone $DOTFILES_URL $DOTFILES_DIR
+
+	link_files
+}
+
+main
+
